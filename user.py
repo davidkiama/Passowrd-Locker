e7200ae964fe4a0add4558968df79997fdc7f675
davidkiama <davidkiama98@gmail.com>
2022-02-18 17:39:39 +0300

    Added functionality to check user password

--- user.py
+++ user.py
@@ -3,3 +3,11 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    @classmethod
+    def check_password(cls, password):
+        if cls.password == password:
+            return True
+        else:
+            print("Wrong password")
+            return False
