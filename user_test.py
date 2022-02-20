a3ae751811a273cda8180b6fe739e3f905436a4d
davidkiama <davidkiama98@gmail.com>
2022-02-21 00:16:31 +0300

    Added test on check_user_pwd

--- user_test.py
+++ user_test.py
@@ -9,12 +9,18 @@ class TestUser(unittest.TestCase):
         """
         self.new_user = User("Davidkiama", "12345678")
 
-    def test__init(self):
+    def test_init(self):
         """
         Test case for the user class
         """
         self.assertEqual(self.new_user.username, "Davidkiama")
         self.assertEqual(self.new_user.password, "12345678")
+
+    def test_user_password(self):
+        """
+        Test case to check if the password is correct
+        """
+        self.assertEqual(self.new_user.check_password("12345678"), True)
 
 
 if __name__ == '__main__':
